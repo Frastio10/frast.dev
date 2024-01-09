84e9cb36d0e59884ca63adc059e2110aeff9e939
Frastio10 <frastio.agustiyan@gmail.com>
2024-01-09 20:16:27 +0700

    NOO DO NOT CLOSE MY SITE!!!!!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,21 +4,28 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 gap-8">
       <div className="text-center">
-        <p>Frastio Agustian</p> <p className="text-lg"> - Software Engineer -</p>
+        <p>Frastio Agustian</p>{" "}
+        <p className="text-lg"> - Software Engineer -</p>
       </div>
       <div className="flex gap-4">
         <Link
           href={"mailto:work.frastioagustian@gmail.com"}
           className="text-blue-200"
+          target="_blank"
         >
           Email
         </Link>
-        <Link href={"https://github.com/frastio10"} className="text-blue-200">
+        <Link
+          href={"https://github.com/frastio10"}
+          className="text-blue-200"
+          target="_blank"
+        >
           GitHub
         </Link>
         <Link
           href={"https://www.linkedin.com/in/frastio-agustian/"}
           className="text-blue-200"
+          target="_blank"
         >
           LinkedIn
         </Link>
